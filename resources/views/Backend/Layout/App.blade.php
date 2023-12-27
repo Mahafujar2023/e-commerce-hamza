b7e906f126e7a193825dc4457fe76e66e2b5967c
Rakib Mahmud <rakibas375@gmail.com>
2023-12-27 23:25:29 +0600

    mastering

---
+++ resources/views/Backend/Layout/App.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        
+        <meta charset="utf-8" />
+        <title>@yield('title')</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <link rel="shortcut icon" href="{{ asset('Backend/assets/images/favicon.ico') }}">
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('Backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{ asset('Backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{ asset('Backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+        @yield('style')
+    </head>
+
+    <body data-sidebar="dark">
+
+    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
+
+            <!-- ==========Header Start ========== -->
+            @include('Backend.Include.Header')
+            <!-- ========== Header Start ========== -->
+
+
+            <!-- ========== Left Sidebar Start ========== -->
+            @include('Backend.Include.Menu')
+            <!-- Left Sidebar End -->
+
+            
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+                        @section('content')
+                    </div>
+                    <!-- container-fluid -->
+                </div>
+                <!-- End Page-content -->
+
+                
+
+               @include('Backend.Include.Footer')
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Right Sidebar -->
+        @include('Backend.Include.Right_sidebar')
+        <!-- /Right-bar -->
+
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('Backend/assets/libs/jquery/jquery.min.js') }}"></script>
+        <script src="{{ asset('Backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+        <script src="{{ asset('Backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+        <script src="{{ asset('Backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+        <script src="{{ asset('Backend/assets/libs/node-waves/waves.min.js') }}"></script>
+
+        <!-- apexcharts -->
+        <script src="{{ asset('Backend/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+
+        <!-- dashboard init -->
+        <script src="{{ asset('Backend/assets/js/pages/dashboard.init.js') }}"></script>
+
+        <!-- App js -->
+        <script src="{{ asset('Backend/assets/js/app.js') }}"></script>
+
+        @yield('script')
+    </body>
+
+</html>
